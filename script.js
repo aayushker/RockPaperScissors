9a5279a39546a64ea4f7a7afa4acae796cf508cf
aayushker <singhaayushker@gmail.com>
2024-02-26 20:01:27 +0530

    Wrote the meta data and wrote comments in the JS file

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 let playerScore = 0;
 let computerScore = 0;
 
+// Function to play the game that is called when the user clicks on the button
+function play(button) {
+    const idButton = button.id;
+    const image = document.getElementById('rotatingImage');
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const scissors = document.getElementById('scissors');
 
+    rock.style.display = 'none';
+    paper.style.display = 'none';
+    scissors.style.display = 'none';
+    image.style.display = 'block';
+
+    setTimeout(function () {
+        image.style.display = 'none';
+        rock.style.display = 'block';
+        paper.style.display = 'block';
+        scissors.style.display = 'block';
+        main(idButton);
+    }, 1200);
+}
+
+// Main logic function for the game
 function main(userChoice) {
     const compChoice = randomSelect();
     const idButton = userChoice;
     var pop = "";
+
+    // Implementing the main logic of the game
     if (userChoice === compChoice) {
         console.log("Tie!");
         pop = "tie";
@@ -40,26 +64,28 @@ function main(userChoice) {
     console.log("Player Score: " + playerScore);
     console.log("Computer Score: " + computerScore);
 
+    // Displaying the scores as a text on the screen
     const playerScoreElement = document.getElementById('playerScore');
     const computerScoreElement = document.getElementById('computerScore');
-
     playerScoreElement.textContent = "Player Score: " + playerScore;
     computerScoreElement.textContent = "Computer Score: " + computerScore;
 
     popup(pop);
 
+    // Displaying the choices of the user and the computer
     const userChoiceElement = document.getElementById('userChoice');
     const computerChoiceElement = document.getElementById('computerChoice');
-    
     userChoiceElement.innerHTML = '';
     computerChoiceElement.innerHTML = '';
 
+    // For the user 
     const userImg = document.createElement('img');
     userImg.src = './assets/' + idButton + '.jpg';
     userImg.style.width = '5vw';
     userImg.style.height = '5vw';
     userChoiceElement.appendChild(userImg);
 
+    // For the computer
     const computerImg = document.createElement('img');
     computerImg.src = './assets/' + compChoice + '.jpg';
     computerImg.style.width = '5vw';
@@ -67,6 +93,7 @@ function main(userChoice) {
     computerChoiceElement.appendChild(computerImg);
 }
 
+// Function to randomly select the choice of the computer
 function randomSelect() {
     const n = Math.floor(Math.random() * 3);
     console.log(n);
@@ -85,27 +112,7 @@ function randomSelect() {
     }
 }
 
-function play(button) {
-    const idButton = button.id;
-    const image = document.getElementById('rotatingImage');
-    const rock = document.getElementById('rock');
-    const paper = document.getElementById('paper');
-    const scissors = document.getElementById('scissors');
-
-    rock.style.display = 'none';
-    paper.style.display = 'none';
-    scissors.style.display = 'none';
-    image.style.display = 'block';
-
-    setTimeout(function () {
-        image.style.display = 'none';
-        rock.style.display = 'block';
-        paper.style.display = 'block';
-        scissors.style.display = 'block';
-        main(idButton);
-    }, 1200);
-}
-
+// Function to reset the game
 function reset() {
     playerScore = 0;
     computerScore = 0;
@@ -116,6 +123,7 @@ function reset() {
     console.clear();
 }
 
+// Function to display the pop-up
 function popup(pop){
     const popText = document.getElementById('myPopup');
     if (pop === "win") {
